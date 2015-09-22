bbc1f9f76e1998c2c1e3761bbfec7deaf198b434
Gahoolee <gahoolee@GahooleedeMacBook-Pro.local>
2015-09-22 22:10:08 +0800

    find_circ flowcart

--- charts.R
+++ charts.R
@@ -1,6 +1,9 @@
 library(DiagrammeR)
 mermaid("
 graph TD
-Reads --> Aignments
-Aignments ==>| discard <br> mapped| B[Remainning Reads]
+Aignments -->| discard full length mapped reads. | B[Remainning Reads]
+B --> | extract. | C[both end 20mers]
+C --> | align. | D[uniq anchor within spliced exons]
+D --> | reverse oriented. | E[circRNA splicing candidates]
+E --> | complete reads align <br> GU/AG splice site |breakpoints
 ")
